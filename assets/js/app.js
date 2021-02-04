0225d8e1dfd9f5a5fa5d79e4bceaaa23db40ef90
Gabriel Oliveira <oliveira.gahenrique@gmail.com>
2021-02-04 10:41:11 -0300

    update: programming phoenix documentation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,11 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
+
+
 import "../css/third-party.scss"
 
 import "../css/app.scss"
-
-
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
